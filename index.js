d21cab214a227707d0604409924894cdc6f08fd9
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-09 07:22:31 -0600

    second commit

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.sendFile('./index.html');
 });
 
-app.get("/lol", (req, res) => {
-  res.send({ hi: "lol" });
-});
 
 const PORT = process.env.PORT || 5000;
 

c633402087ee0583c338663d31acb72e6a9e0bd4
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-12 10:02:03 -0600

    More work on oauth and passport

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ passport.use(
   )
 );
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server started.");
-});
-
-app.get(
+app.get(//
   "/auth/google",
   passport.authenticate("google", {
     scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server started.");
+});

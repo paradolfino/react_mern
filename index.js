ca8b0c77ce554fd4d82c2dd13540ee36ca20a994
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-18 09:40:19 -0600

    add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongooseURI,()=>{
+  console.log('Database connection established.');
+});
 
 const app = express();
 

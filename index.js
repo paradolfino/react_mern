ec41c6682d76afba1006ab3e28fde083e968eca3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-21 19:03:14 -0600

    fixed mongoose connection issues

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongooseURI,()=>{
-  console.log('Database connection established.');
-});
 
+mongoose.connect(keys.mongoURI,{ useMongoClient: true });
+mongoose.promise = global.Promise;
 const app = express();
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("Database Connected");
+});
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,32 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback" //route user sent to after auth
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log('access token: ',accessToken);
-        console.log('refresh token: ', refreshToken);
-        console.log('profile: ', profile);
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback" //route user sent to after auth
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          newUser = new User({
+            googleId: profile.id
+          });
+
+          newUser.save(function(err) {
+            if (err) throw err;
+          })
+          .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);

a3f7e33d2c36c778694b9384bb65fda90fda4760
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-30 17:05:54 -0600

    commit 2 to pool[pretty-jumping-rivers-6kf2v4] at 17:05 - 30/12/2017

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Header</h2>;
+const SurveyNew = () => <h2>Header</h2>;
+const Landing = () => <h2>Header</h2>;
+
 const App = () => {
     return (
         <div>

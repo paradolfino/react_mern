295db4ecc2d092a37746443a2e490d357a99c95a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-31 14:23:24 -0600

    commit 5 to pool[rapid-shining-mountains-q5e443] at 14:23 - 31/12/2017

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Header
-            </div>
-        );
-    }
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">
+            Logo
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
